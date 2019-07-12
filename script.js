9d99eaa7625c95fe414e50a6310036a7650c08be
Alex Chadwick <alexchadwick@Alexs-MacBook-Pro-7.local>
2019-07-12 16:07:26 -0400

    .

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() { 
-  // write your javascript code below.
-
-
-
+var color= prompt("Pick a color Blue or Pink or Purple");
+color=color.toLowerCase();
+if (color=="blue"){
+	alert("You will get Alex a $2000 PC");
+}if (color=="pink"){
+	alert("You will get Alex a $1500 PC");
+}if (color=="purple"){
+	alert("This color is under maintence pick another color and try agian")
+}
 });
